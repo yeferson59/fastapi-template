063311965701eeb1443931a433f795eb35702903
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-06-11 23:16:50 -0500

    feat(database): update User model fields

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,11 @@
-from sqlmodel import SQLModel, Field, String
-from typing_extensions import Optional
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
 
 class User(SQLModel, table=True):
-    id: Optional[str] = Field(default=None, primary_key=True, index=True)
-    name: str = Field(String, index=True)
-    email: str = Field(String, unique=True, index=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
+    email: str = Field(unique=True, index=True)
+    is_active: bool = Field(default=True)
+    is_superuser: bool = Field(default=False)
+    full_name: Optional[str] = None
