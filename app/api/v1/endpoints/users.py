eb90b3650b148994f9506164a457cb4b3f04dfdb
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-06-11 22:40:24 -0500

    feat(api): switch to database query for user retrieval

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from typing import Annotated
+from sqlmodel import Session
+from app.api.deps import session_local
+from app.models.user import User
+from sqlmodel import select
 
 router = APIRouter()
 
+SessionDep = Annotated[Session, Depends(session_local)]
+
 @router.get("/")
-def read_users():
-    return [{"username": "user1"}, {"username": "user2"}]
+def read_users(db: SessionDep):
+    user = db.exec(select(User)).all()
+    return {"users": user}
