d0371abaea2b6825acc5b675fb5c78112a20c102
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-06-13 00:58:54 -0500

    chore(comments): remove unnecessary comments and update Pydantic config

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,9 +1,6 @@
 from typing import Optional
-
 from pydantic import BaseModel, EmailStr
 
-
-# Shared properties
 class UserBase(BaseModel):
     email: Optional[EmailStr] = None
     is_active: Optional[bool] = True
@@ -11,12 +8,10 @@ class UserBase(BaseModel):
     full_name: Optional[str] = None
 
 
-# Properties to receive via API on creation
 class UserCreate(UserBase):
     password: str
 
 
-# Properties to receive via API on update
 class UserUpdate(UserBase):
     password: Optional[str] = None
 
@@ -24,15 +19,14 @@ class UserUpdate(UserBase):
 class UserInDBBase(UserBase):
     id: Optional[int] = None
 
-    class Config:
-        orm_mode = True
+    model_config = {
+        "from_attributes": True
+    }
 
 
-# Additional properties to return via API
 class User(UserInDBBase):
     pass
 
 
-# Additional properties stored in DB
 class UserInDB(UserInDBBase):
     hashed_password: str
