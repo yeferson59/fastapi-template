a8fab881e2b221d0cca4f4e31b3eb57c769a6d63
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-06-13 11:04:03 -0500

    chore(format): indentation in get_db function

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,8 @@
 from sqlmodel import Session
+
 from app.db.base import engine
 
+
 def get_db():
-  with Session(engine) as session:
-      yield session
+    with Session(engine) as session:
+        yield session
