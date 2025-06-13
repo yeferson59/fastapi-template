57433b2843d323c5f3c98d4e5da0107081d3f81b
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-06-13 00:58:32 -0500

    fix(model): update User model to inherit from SQLModelWithId

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,12 @@
 from typing import Optional
-
 from sqlmodel import Field, SQLModel
+from app.crud.base import SQLModelWithId
 
 
-class User(SQLModel, table=True):
+class User(SQLModelWithId, SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     email: str = Field(unique=True, index=True)
     is_active: bool = Field(default=True)
     is_superuser: bool = Field(default=False)
     full_name: Optional[str] = None
+    password: str = Field()
