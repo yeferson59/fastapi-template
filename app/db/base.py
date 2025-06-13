263fe3f5cede565a82adcdec3232712343aef614
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-06-13 11:03:06 -0500

    chore(format): Reorder imports and format connect_args dictionary

--- app/db/base.py
+++ app/db/base.py
@@ -1,12 +1,10 @@
-from sqlmodel import (create_engine, SQLModel)
+from sqlmodel import SQLModel, create_engine
+
 from app.core.config import get_settings
 
 settings = get_settings()
 
-connect_args = {
-    "check_same_thread": False,
-    "auth_token": settings.turso_auth_token
-}
+connect_args = {"check_same_thread": False, "auth_token": settings.turso_auth_token}
 
 engine = create_engine(
     f"sqlite+{settings.database_url}?secure=true",
@@ -14,6 +12,7 @@ engine = create_engine(
     echo=settings.debug,
     connect_args=connect_args,
 )
+
 
 def init_db():
     """Inicializa la base de datos creando todas las tablas"""
