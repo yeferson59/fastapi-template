c19609c6bb5a0ec7539085f3d006f1af320396cd
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-06-13 11:05:12 -0500

    chore(format): add health check endpoint to main application

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+
 from app.api.v1.api import api_router
 from app.core.config import get_full_api_prefix
 from app.db.base import init_db
@@ -6,12 +7,15 @@ from app.db.base import init_db
 app = FastAPI()
 prefix = get_full_api_prefix()
 
+
 @app.on_event("startup")
 def on_startup():
     init_db()
 
+
 @app.get("/health")
 async def health_check():
     return {"status": "healthy", "message": "Service is running smoothly"}
+
 
 app.include_router(api_router, prefix=prefix)
