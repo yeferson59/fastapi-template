0beafa2974eb6c9813f08fc9347f57035325fdc2
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-06-17 19:17:23 -0500

    feat(test): add uvicorn to run FastAPI app debuging mode

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 
+import uvicorn
 from fastapi import FastAPI
 
 from app.api.v1.api import api_router
@@ -23,3 +24,6 @@ async def health_check():
 
 
 app.include_router(api_router, prefix=prefix)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
