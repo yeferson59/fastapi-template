6dabfa0e03d780d24d9e463c8baf8681fe6696ae
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-10-06 20:44:06 -0500

    feat: specify int type for SQLModelWithId primary key

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,5 @@
-from typing import Any
-
 from sqlmodel import Field, SQLModel
 
 
 class SQLModelWithId(SQLModel):
-    id: Any = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
